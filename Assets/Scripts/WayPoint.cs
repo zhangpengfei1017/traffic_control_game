f1de159916236ee8683826741de936093194921a
Chi Shen <Chi Shen>
2016-09-02 12:30:28 -0400

    add some types of waypoint

---
+++ Assets/Scripts/WayPoint.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class WayPoint : MonoBehaviour
+{
+    public WayPoint[] m_Outbounds;
+
+    protected Color m_gizmosColor = Color.blue;
+
+    protected virtual void OnDrawGizmos()
+    {
+        Gizmos.color = m_gizmosColor;
+        Gizmos.DrawSphere(transform.position, 0.5f);
+
+        if (null == m_Outbounds)
+        {
+            return;
+        }
+
+        Gizmos.color = Color.yellow;
+        for (int i = 0; i < m_Outbounds.Length; i++)
+        {
+            Gizmos.DrawLine(transform.position, m_Outbounds[i].transform.position);
+        }
+    }
+}
